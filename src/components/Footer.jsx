a2260e9c88c5c1d421a5dcc915dc95a911e4f559
IQRAR-LUQMAN <146273811+Iqrarluqman602@users.noreply.github.com>
2024-05-22 13:37:56 -0700

    Update Footer.jsx
    sdfdsfd

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -9,7 +9,7 @@ const Footer = () => {
 
       <div className='footer-container'>
         <p>
-          © 2024 <strong>IqrarWebDiv</strong>. All rights reserved.
+          © 2024 <strong>PixelCraft</strong>. All rights reserved.
         </p>
 
         <div className='flex gap-3 justify-center items-center'>
