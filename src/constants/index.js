d1f0a88ce6d7e2c32964ebdb1a08027acf6dabdf
raoiqrar918@gmail.com <raoiqrar918@gmail.com>
2024-06-06 20:47:33 -0700

    first coments

--- src/constants/index.js
+++ src/constants/index.js
@@ -136,7 +136,7 @@ export const experiences = [
         title: "Teaching as a Front-End Developer",
         company_name: "ARENA Multimedia Educational institution in Karachi",
         icon: tesla,
-        iconBg: "#fbc3bc",
+        iconBg: "#accbe1",
         date: "Jan 2021 - Feb 2022",
         points: [
             "1 year experience as Front-end developer Teacher in ARENA Multimedia Educational institution in Karachi.",
